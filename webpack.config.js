d2ae12cff2be965bb16b63d7c9e52eb2578fb8f4
Pranav Kalvapalle <pranavkalvapalle@pranavs-mbp.gateway.sonic.net>
2016-05-29 00:03:20 -0700

    Initial build tool specifications - add config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,18 @@ const path    = require('path'),
 
 const getConfig = require('hjs-webpack');
 
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest    = join(root, 'dist');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
 var config = getConfig({
-  in: join(__dirname, 'src/app.js'),
-  out: join(__dirname, 'dist')
-})
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+});
+
+module.exports = config;
